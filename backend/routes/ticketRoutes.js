26488037d7e4d1c953edea6a5de06cf11fe4ace2
Simodalstix <lastpolar@gmail.com>
2022-04-16 10:04:05 +1000

    get and create tickets (backend)

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const { getTickets, createTicket } = require('../controllers/ticketController');
 
-const {protect} = require('../middleware/authMiddleware')
-router.route('/').get(protect, getTickets).post(protect, createTicket)
+const { protect } = require('../middleware/authMiddleware');
+router.route('/').get(protect, getTickets).post(protect, createTicket);
 
-module.exports = router
+module.exports = router;
